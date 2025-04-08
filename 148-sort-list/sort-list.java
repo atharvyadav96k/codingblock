c312881cbb164a026e25f3079482c99acd18ac82
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2025-04-08 08:17:37 +0530

    Time: 9 ms (94.61%) | Memory: 57.1 MB (38.01%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,32 @@
+class Solution {
+    ListNode merge(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = (l1 != null) ? l1 : l2;
+        return head.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return merge(left, right);
+    }
+}
